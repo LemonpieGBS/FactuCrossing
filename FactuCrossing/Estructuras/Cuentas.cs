79d2fe752da313c37de716224a4e2e108ca8bfd7
LemonpieGBS <labc202@uam.net.ni>
2024-11-14 10:00:43 -0600

    Arreglar cosas del framework y agregar structs

---
+++ FactuCrossing/Estructuras/Cuentas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactuCrossing.Estructuras
+{
+    public enum Roles
+    {
+        FACTURISTA,
+        GESTORDEINVENTARIO,
+        ANALISTA,
+        ADMINISTRADOR,
+        GERENTE
+    }
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Hash { get; set; }
+        public string Salt { get; set; }
+        public Roles Rol { get; set; }
+    }
+}
---
+++ FactuCrossing/Estructuras/Inventario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactuCrossing.Estructuras
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public decimal Precio { get; set; }
+        public int CantidadEnStock { get; set; }
+    }
+
+    public class Descuento
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Porcentaje { get; set; }
+    }
+}
